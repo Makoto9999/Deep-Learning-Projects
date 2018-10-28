9547ed6d6756bbb438a8ccca3cdd4ba4fbb483ca
Zezheng Long <38631191+Makoto9999@users.noreply.github.com>
2018-10-28 10:01:27 -0500

    Update visualize_cv.py

--- Mask-RCNN/visualize_cv.py
+++ Mask-RCNN/visualize_cv.py
@@ -121,7 +121,7 @@ if __name__ == '__main__':
         )
 
         cv2.imshow('frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'): """ Quit with 'q' """
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     capture.release()
